b5b98fcf3192f95b87f02a9f57da543201fb4675
Aditya Vishwakarma <adityaarmy007@gmail.com>
2026-07-21 20:53:03 +0530

    Added Lombok to reduce Code

--- src/main/java/com/firstLearning/journalPrototype/entity/JournalEntry.java
+++ src/main/java/com/firstLearning/journalPrototype/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.firstLearning.journalPrototype.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +12,7 @@ import java.time.LocalDateTime;
 //Remainder : Controller --> Service --> Repository
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -19,29 +23,5 @@ public class JournalEntry {
     private String content;
 
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;}
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;}
-
-    public String getTitle() {
-        return title;}
-
-    public void setTitle(String title) {
-        this.title = title;}
-
-    public String getContent() {
-        return content;}
-
-    public void setContent(String content) {
-        this.content = content;}
-
-    public ObjectId getId() {
-        return id;}
-
-    public void setId(ObjectId id) {
-        this.id = id;}
 
 }
